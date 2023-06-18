700a596acd0db53a84b5ea5b41920bca5896104c
Ahbari-M <mohammed.ahbari@usmba.ac.ma>
2023-06-18 20:19:17 +0100

    fix expressjwt middleware

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import http from 'http' 
 import cors from 'cors';
-import router from './routes/user.js';
-import expressjwt from 'express-jwt'
+import router from './routes/api.js';
+import {expressjwt} from 'express-jwt'
 import handleError from './shared/handleError.js';
 import config from './config.js';
 
+const {secret} = config
 const app = express()
 const server = http.createServer(app);
 
@@ -15,15 +16,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 // use JWT auth to secure the api
-app.use( () => {
-  const { secret } = config;
-  return expressjwt({ secret, algorithms: ['HS256'] }).unless({
+app.use(expressjwt({ secret, algorithms: ['HS256'] }).unless({
       path: [
           // public routes that don't require authentication
-          'api/users/signin'
+          '/api/users/signin',
+          '/api/users/signup',
+          '/api/users/all'
       ]
-  });
-})
+  })
+)
 
 // routes
 app.use('/api', router)
