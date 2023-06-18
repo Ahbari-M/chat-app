8610136f550dc445c58ec6020e07555dabc025ce
Ahbari-M <mohammed.ahbari@usmba.ac.ma>
2023-06-18 20:20:08 +0100

    update err handling

--- backend/src/shared/handleError.js
+++ backend/src/shared/handleError.js
@@ -2,6 +2,7 @@
 
 export default function handleError(err, req, res, next) {
     if (typeof (err) === 'string') {
+        console.log(err)
         // custom application error
         return res.status(400).json({ message: err });
     }
@@ -10,7 +11,7 @@ export default function handleError(err, req, res, next) {
         // jwt authentication error
         return res.status(401).json({ message: 'Invalid Token' });
     }
-
+    console.log(err?.stack)
     // default to 500 server error
     return res.status(500).json({ message: err.message });
 }
