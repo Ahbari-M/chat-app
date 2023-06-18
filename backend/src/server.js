cb808803460f3c68892925a5ecfee89ecb235557
Ahbari-M <mohammed.ahbari@usmba.ac.ma>
2023-06-18 19:47:14 +0100

    adding middlewares

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,35 @@
 import express from 'express'
 import http from 'http' 
+import cors from 'cors';
+import router from './routes/user.js';
+import expressjwt from 'express-jwt'
+import handleError from './shared/handleError.js';
+import config from './config.js';
 
 const app = express()
 const server = http.createServer(app);
 
-// respond with "hello world" when a GET request is made to the homepage
-app.get('/', (req, res) => {
-  res.send('hello world')
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+// use JWT auth to secure the api
+app.use( () => {
+  const { secret } = config;
+  return expressjwt({ secret, algorithms: ['HS256'] }).unless({
+      path: [
+          // public routes that don't require authentication
+          'api/users/signin'
+      ]
+  });
 })
+
+// routes
+app.use('/api', router)
+
+// Error handling
+app.use(handleError);
 
 export default server 
 
