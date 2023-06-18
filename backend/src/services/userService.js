b7410318c35b4046831231de55db9b89685e7aad
Ahbari-M <mohammed.ahbari@usmba.ac.ma>
2023-06-18 20:19:49 +0100

    user service

---
+++ backend/src/services/userService.js
@@ -0,0 +1,45 @@
+import bcrypt from 'bcrypt';
+
+const users = []
+
+async function signup(data) {
+  const user = {
+      userName: data.userName,
+      password: bcrypt.hashSync(data.password, 8),
+  }
+
+  if (users.findIndex(user => user.userName === data.userName) !== -1) {
+    throw 'user name already existe'
+  }
+
+  users.push(user)
+
+  return user.userName;
+}
+
+
+async function signin(data) {
+  const user = users.find(u => u.userName === data.userName)
+  
+  if (!user) {
+    throw 'user not found'
+  }
+  
+  if(bcrypt.compareSync(data.password, user?.password || '')){
+      const {password, ...userWithoutPwd} = user
+      return userWithoutPwd;
+    }
+    else{
+      throw 'invalid information';
+    }
+  }
+  
+async function getAll() { 
+  return users.map(user => ({userName: user.userName}))
+}
+
+export default {
+  signup,
+  signin,
+  getAll
+}
