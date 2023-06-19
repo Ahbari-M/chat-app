caf8f0a56f9f266658c394881dfdd2b0cede53b5
Ahbari-M <mohammed.ahbari@usmba.ac.ma>
2023-06-19 13:35:03 +0100

    jwt utils

---
+++ frontend/src/utils/jwt.js
@@ -0,0 +1,13 @@
+import axios from './axios';
+
+
+export const setSession = (accessToken) => {
+  if (accessToken) {
+    localStorage.setItem('accessToken', accessToken);
+    axios.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
+  } else {
+    localStorage.removeItem('accessToken');
+    delete axios.defaults.headers.common.Authorization;
+  }
+};
+
