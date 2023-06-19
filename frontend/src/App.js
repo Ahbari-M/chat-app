207fcf502ad8bcb55325e168f4dcc57c9e779539
Ahbari-M <mohammed.ahbari@usmba.ac.ma>
2023-06-19 14:21:58 +0100

    update app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
+import { Card, Container } from "@mui/material";
+import ChatSidebar from "./components/ChatSidebar";
+import ChatWindow from "./components/ChatWindow";
+import { Route, Routes } from "react-router-dom";
+import Chat from "./pages/Chat";
+import SignIn from "./pages/Signin";
+import Signup from "./pages/Signup";
 
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <Routes>
+      <Route index path="/" element={<Chat />} />
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/signup" element={<Signup />} />
+   </Routes>
   );
 }
 
