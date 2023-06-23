8127181301d48287ae7b2a684733a612663313b2
Ahbari-M <mohammed.ahbari@usmba.ac.ma>
2023-06-23 18:50:39 +0100

    add header for char component

--- frontend/src/components/ChatWindow.jsx
+++ frontend/src/components/ChatWindow.jsx
@@ -1,17 +1,21 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate, useParams, useLocation } from 'react-router-dom';
 // @mui
 import { Box, Divider, Stack } from '@mui/material';
+import useSocket from '../hooks/useSocket';
+import ChatHeader from './ChatHeader';
 
 // ----------------------------------------------------------------------
 
 
 export default function ChatWindow() {
+  const [chatMembers, setChatMembers] = useState([]) 
+  const { onlineUsers} = useSocket();
 
   return (
     <Stack sx={{ flexGrow: 1, minWidth: '1px' }}>
         
-
+      <ChatHeader contacts={onlineUsers} recipients={chatMembers} onAddRecipients={(users) => setChatMembers(users) } />
 
       <Divider />
 
