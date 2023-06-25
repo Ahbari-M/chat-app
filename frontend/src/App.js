fc26881e773322902aff747744cb60610d6e2b43
Ahbari-M <mohammed.ahbari@usmba.ac.ma>
2023-06-25 21:06:05 +0100

    update routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,16 @@ import { Route, Routes } from "react-router-dom";
 import Chat from "./pages/Chat";
 import SignIn from "./pages/Signin";
 import Signup from "./pages/Signup";
+import ChatRoom from "./components/ChatRoom";
 
 
 function App() {
   return (
     <Routes>
-      <Route index path="/" element={<Chat />} />
+      <Route path="/" element={<Chat />}>
+        <Route index path="/" element={'new chat room'} />
+        <Route path="/chat/:id" element={<ChatRoom />} />
+      </Route>
       <Route path="/signin" element={<SignIn />} />
       <Route path="/signup" element={<Signup />} />
    </Routes>
